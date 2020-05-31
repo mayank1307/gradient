37f4843f1e295c8fab26916163cbcf797dace469
mayank1307 <mayankku1307@gmail.com>
2020-05-31 18:05:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import htmlToImage from 'html-to-image';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Modal from 'react-bootstrap/Modal';
 
 class App extends React.Component {
   constructor(){
@@ -10,6 +11,8 @@ class App extends React.Component {
     this.state={
       NumberHolder:0,
       dirC:0,
+      show:false,
+      setShow:false,
       textSizes:50,
       textData:"",
       value: '',
@@ -45,48 +48,68 @@ class App extends React.Component {
     }
   }
   ImageC=()=>{
-  htmlToImage.toJpeg(document.getElementById('boxG'), { quality: 1 })
-  .then(function (dataUrl) {
-    var link = document.createElement('a');
-    link.download = 'ui-grad.jpeg';
+    htmlToImage.toJpeg(document.getElementById('boxG'), { quality: 1 })
+    .then(function (dataUrl) {
+      var link = document.createElement('a');
+      link.download = 'ui-grad.jpeg';
     link.href = dataUrl;
     link.click();
   });
-  } 
-  copyCodeToClipboard = () => {
-    const el = this.textArea
-    el.select()
-    document.execCommand("copy")
-  }
-  ChangeText=()=>{
-    var newText=document.getElementById('textBox').value;
-    this.setState({
-      textData:newText
-    });
-  }
-  DeleteText=()=>{
-    document.getElementById('textBox').value="";
-    this.setState({textData:"",textSizes:"50"});
-  }
-  DecText=()=>{
+} 
+copyCodeToClipboard = () => {
+  const el = this.textArea
+  el.select()
+  document.execCommand("copy")
+}
+ChangeText=()=>{
+  var newText=document.getElementById('textBox').value;
+  this.setState({
+    textData:newText
+  });
+}
+DeleteText=()=>{
+  document.getElementById('textBox').value="";
+  this.setState({textData:"",textSizes:"50"});
+}
+DecText=()=>{
     this.setState({textSizes:this.state.textSizes-10})
   }
   IncText=()=>{
     this.setState({textSizes:this.state.textSizes+10});
   }
+  handleClose = () => {this.setState({show:false})};
+  handleShow = () => {this.setState({show:true})};
+  
   render(){
-  return (
-    <div className="App mx-1 my-1">
+    return (
+      <div className="App mx-1 my-1">
       <div className="box" id="boxG" style={{background:this.state.colors,height:"450px"}}><h5 className="font-weight-bold text-light px-5 py-5" style={{fontSize:this.state.textSizes+"px",alignItems:"center",justifyContent: "center",textShadow:"2px 2px 8px #000000",display: "flex"}}>{this.state.textData}</h5></div><br/>
-      <Button className="mx-1 px-3" variant="outline-danger" onClick={this.GenerateG}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M2 7h-2v-2h2c3.49 0 5.48 1.221 6.822 2.854-.41.654-.754 1.312-1.055 1.939-1.087-1.643-2.633-2.793-5.767-2.793zm16 10c-3.084 0-4.604-1.147-5.679-2.786-.302.627-.647 1.284-1.06 1.937 1.327 1.629 3.291 2.849 6.739 2.849v3l6-4-6-4v3zm0-10v3l6-4-6-4v3c-5.834 0-7.436 3.482-8.85 6.556-1.343 2.921-2.504 5.444-7.15 5.444h-2v2h2c5.928 0 7.543-3.511 8.968-6.609 1.331-2.893 2.479-5.391 7.032-5.391z"/></svg></Button>
-      <Button className="mx-1 px-3" variant="outline-dark" onClick={this.ChangeDir}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21 9h2v11h-17v3l-6-4 6-4v3h15v-9zm-18-3h15v3l6-4-6-4v3h-17v11h2v-9z"/></svg></Button>
-      <Button className="mx-1 px-3" variant="outline-success" onClick={this.ImageC}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17 13v-13h-2v5h-2v-3h-2v7h-2v-9h-2v13h-6l11 11 11-11z"/></svg></Button>
-      <Button className="mx-1 px-3" variant="outline-info" onClick={() => this.copyCodeToClipboard()}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z"/></svg></Button>
+      <Button className="mx-1 px-2" variant="outline-light" onClick={this.GenerateG}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10.024 4h6.015l7.961 8-7.961 8h-6.015l7.961-8-7.961-8zm-10.024 16h6.015l7.961-8-7.961-8h-6.015l7.961 8-7.961 8z"/></svg></Button>
+      <Button className="mx-1 px-2" variant="outline-light" onClick={this.ChangeDir}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm16.168-3.168l1.832-1.832v6h-6l1.789-1.789c-4.382-3.121-10.677 1.802-5.011 7.706-8.822-5.606-.456-16.155 7.39-10.085z"/></svg></Button>
+      <Button className="mx-1 px-2" variant="outline-light" onClick={this.ImageC}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17 13v-13h-2v5h-2v-3h-2v7h-2v-9h-2v13h-6l11 11 11-11z"/></svg></Button>
+      <Button className="mx-1 px-2" variant="outline-light" onClick={() => this.copyCodeToClipboard()}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z"/></svg></Button>
+      <Button variant="outline-light" onClick={this.handleShow}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.033 16.01c.564-1.789 1.632-3.932 1.821-4.474.273-.787-.211-1.136-1.74.209l-.34-.64c1.744-1.897 5.335-2.326 4.113.613-.763 1.835-1.309 3.074-1.621 4.03-.455 1.393.694.828 1.819-.211.153.25.203.331.356.619-2.498 2.378-5.271 2.588-4.408-.146zm4.742-8.169c-.532.453-1.32.443-1.761-.022-.441-.465-.367-1.208.164-1.661.532-.453 1.32-.442 1.761.022.439.466.367 1.209-.164 1.661z"/></svg>
+      </Button>
+      <Modal show={this.state.show} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={this.handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <InputGroup className="px-1 row">
         <div className="col-12 py-1 col-md-6"><FormControl placeholder="Add text" onChange={this.ChangeText} id="textBox"/></div>
-        <div className="py-1 px-3"><Button className="" onClick={this.DeleteText} variant="outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/></svg></Button></div>
-        <div className="py-1 px-1"><Button className="" onClick={this.DecText} variant="outline-info"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z"/></svg></Button></div>
-        <div className="py-1 px-1"><Button className="" onClick={this.IncText} variant="outline-info"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg></Button></div>
+        <div className="py-1"><Button onClick={this.DeleteText} variant="outline-light"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z" fill="red"/></svg></Button></div>
+        <div className="py-1"><Button onClick={this.DecText} variant="outline-light"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" fill="teal" /></svg></Button></div>
+        <div className="py-1"><Button onClick={this.IncText} variant="outline-light"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" fill="teal" /></svg></Button></div>
       </InputGroup>
       <textarea ref={(textarea) => this.textArea = textarea} style={{opacity:"0",width:"0"}} value={"background:"+this.state.colors+";"}/>
     </div>
